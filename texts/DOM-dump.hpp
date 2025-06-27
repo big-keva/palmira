faa2621554560a19f233eb101fafc1a8fbc5bb90
Keva <keva@rambler.ru>
2025-06-27 12:11:54 +0300

    move file

--- texts/DOM-dump.hpp
+++ texts/DOM-dump.hpp
@@ -1,14 +1,17 @@
-# if !defined( __palmira_texts_DOM_sink_hpp__ )
-# define __palmira_texts_DOM_sink_hpp__
+# if !defined( __palmira_texts_DOM_dump_hpp__ )
+# define __palmira_texts_DOM_dump_hpp__
 # include "DOMini.hpp"
+# include <moonycode/codes.h>
 # include <mtc/serialize.h>
+# include <functional>
+# include <stdexcept>
 # include <memory>
 
 namespace palmira {
 namespace texts   {
 namespace dump_as {
 
-  auto  Tags( std::function<void(const char*, size_t)> ) -> mtc::api<IText>;
+  auto  Tags( std::function<void(const char*, size_t)>, unsigned encode = codepages::codepage_utf8 ) -> mtc::api<IText>;
   auto  Json( std::function<void(const char*, size_t)> ) -> mtc::api<IText>;
 
   template <class O>
@@ -23,4 +26,4 @@ namespace dump_as {
 
 }}}
 
-# endif // !__palmira_texts_DOM_sink_hpp__
+# endif // !__palmira_texts_DOM_dump_hpp__
