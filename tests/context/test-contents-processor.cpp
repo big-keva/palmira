857aab11b90cdc9461c586778f7d1b8bf5ad9fe4
Keva <keva@rambler.ru>
2025-07-14 20:28:02 +0300

    files moved

---
+++ tests/context/test-contents-processor.cpp
@@ -0,0 +1,125 @@
+# include "context/decomposer.hpp"
+# include "context/contents-processor.hpp"
+# include "texts/DOM-text.hpp"
+# include "src/texts/text-2-image.hpp"
+# include <mtc/test-it-easy.hpp>
+
+using namespace palmira;
+using namespace palmira::query;
+using namespace palmira::texts;
+
+auto  StrKey( const char* str ) -> Key
+{
+  return { 0xff, codepages::mbcstowide( codepages::codepage_utf8, str ) };
+}
+
+TestItEasy::RegisterFunc  test_contents_processor( []()
+{
+  auto  text = Document();
+    Document{
+      { "title", {
+          "Сказ про радугу",
+          "/",
+          "Rainbow tales" } },
+      { "body", {
+          "Как однажды Жак Звонарь"
+          "Городской сломал фонарь" } },
+      "Очень старый фонарь" }.CopyUtf16( &text );
+  auto  body = BreakWords( text.GetBlocks() );
+    CopyMarkup( body, text.GetMarkup() );
+
+  TEST_CASE( "query/Decomposer" )
+  {
+    SECTION( "DefaultDecomposer works without any linguistics" )
+    {
+      Decomposer        decomposer;
+
+      SECTION( "decomposer may be created" )
+      {
+        if ( REQUIRE_NOTHROW( decomposer = DefaultDecomposer() ) )
+        {
+          auto  contents = mtc::api<IImage>();
+
+          SECTION( "decomposer fills images:" )
+          {
+            SECTION( "* as simple keys index")
+            {
+              if ( REQUIRE_NOTHROW( contents = mtc::api<IImage>( new coord::NullContents() ) )
+                && REQUIRE( contents != nullptr ) )
+              {
+                REQUIRE_NOTHROW( decomposer( contents,
+                  body.GetTokens(),
+                  body.GetMarkup() ) );
+
+                REQUIRE_NOTHROW( contents->List( [&]( const Span&, const Span& value, unsigned bkType )
+                  {
+                    REQUIRE( bkType == 0 );
+                    REQUIRE( value.size() == 0 );
+                  } ) );
+              }
+            }
+            SECTION( "* as BM25 index")
+            {
+              if ( REQUIRE_NOTHROW( contents = mtc::api<IImage>( new coord::BM25Contents() ) )
+                && REQUIRE( contents != nullptr ) )
+              {
+                REQUIRE_NOTHROW( decomposer( contents,
+                  body.GetTokens(),
+                  body.GetMarkup() ) );
+
+                REQUIRE_NOTHROW( contents->List( [&]( const Span& key, const Span& value, unsigned bkType )
+                  {
+                    if ( REQUIRE( bkType == EntryBlockType::EntryCount ) && REQUIRE( value.size() != 0 ) )
+                    {
+                      int ncount;
+
+                      ::FetchFrom( value.data(), ncount );
+
+                      if ( Key( key.data(), key.size() ) == StrKey( "фонарь" ) )
+                        REQUIRE( ncount == 1 );
+                      else
+                        REQUIRE( ncount == 0 );
+                    }
+                  } ) );
+              }
+            }
+            SECTION( "* as detailed index")
+            {
+              if ( REQUIRE_NOTHROW( contents = mtc::api<IImage>( new coord::DetailedContents() ) )
+                && REQUIRE( contents != nullptr ) )
+              {
+                REQUIRE_NOTHROW( decomposer( contents,
+                  body.GetTokens(),
+                  body.GetMarkup() ) );
+
+                REQUIRE_NOTHROW( contents->List( [&]( const Span& key, const Span& value, unsigned bkType )
+                  {
+                    if ( REQUIRE( bkType == EntryBlockType::EntryOrder ) && REQUIRE( value.size() != 0 ) )
+                    {
+                      int   getpos;
+                      int   bkSize;
+                      auto  source = ::FetchFrom( ::FetchFrom( value.data(),
+                        bkSize ),
+                        getpos );
+
+                      if ( Key( key.data(), key.size() ) == StrKey( "радугу" ) )
+                      {
+                        REQUIRE( getpos == 2 );
+                      }
+                        else
+                      if ( Key( key.data(), key.size() ) == StrKey( "фонарь" ) )
+                      {
+                        REQUIRE( getpos == 11 );
+                          ::FetchFrom( source, getpos );
+                        REQUIRE( getpos == 2 );
+                      }
+                    }
+                  } ) );
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+} );
