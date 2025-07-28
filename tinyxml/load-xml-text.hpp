0d7b6cc80d1dc865373a3ff6c14a1561d14a1d29
Keva <keva@rambler.ru>
2025-07-28 19:42:12 +0300

    tinyxml reader added

---
+++ tinyxml/load-xml-text.hpp
@@ -0,0 +1,22 @@
+# if !defined( __palmira_tinyxml_load_xml_text_hpp__ )
+# define __palmira_tinyxml_load_xml_text_hpp__
+# include "DelphiX/text-api.hpp"
+
+namespace palmira {
+namespace tinyxml {
+
+  enum class TagMode: unsigned
+  {
+    undefined = 0,
+    ignore = 1,     // ignore tag
+    remove = 2      // suppress contents
+  };
+
+  using TagModes = std::initializer_list<std::pair<const std::string, TagMode>>;
+
+  void  LoadObject( DelphiX::textAPI::IText*, const TagModes&, const char*, size_t );
+  void  LoadObject( DelphiX::textAPI::IText*, const TagModes&, FILE* );
+
+}}
+
+# endif   // !__palmira_tinyxml_load_xml_text_hpp__
