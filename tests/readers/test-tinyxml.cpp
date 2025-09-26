2325a262289aa4c48aa5e166a28380afde0fbfe2
Keva <keva@rambler.ru>
2025-09-26 16:17:35 +0300

    newly added

---
+++ tests/readers/test-tinyxml.cpp
@@ -0,0 +1,42 @@
+# include <DelphiX/textAPI/DOM-text.hpp>
+# include <DelphiX/textAPI/DOM-dump.hpp>
+# include "../../readers/read-xml.hpp"
+# include <mtc/test-it-easy.hpp>
+
+auto  LoadFile( const char* path ) -> std::vector<char>
+{
+  std::vector<char> load;
+  char              buff[1024];
+  auto              file = fopen( path, "rb" );
+
+  for ( auto read = fread( buff, 1, sizeof(buff), file ); read > 0; read = fread( buff, 1, sizeof(buff), file ) )
+    load.insert( load.end(), buff, buff + read );
+
+  fclose( file );
+
+  return load;
+}
+
+TestItEasy::RegisterFunc  test_tinyxml( []()
+{
+  TEST_CASE( "tinyxml/load" )
+  {
+    SECTION( "xml may be loaded as text" )
+    {
+      auto  text = DelphiX::textAPI::Document();
+      auto  buff = LoadFile( "/home/keva/dev/palmira/texts/panov_demon.fb2" );
+
+      palmira::tinyxml::Read( &text, {
+        { "empty-line", palmira::tinyxml::TagMode::remove },
+        { "coverpage",  palmira::tinyxml::TagMode::remove },
+        { "binary",     palmira::tinyxml::TagMode::remove },
+        { "image",      palmira::tinyxml::TagMode::remove },
+        { "section",    palmira::tinyxml::TagMode::ignore },
+        { "FictionBook",palmira::tinyxml::TagMode::ignore },
+        { "p",          palmira::tinyxml::TagMode::ignore } }, buff.data(), buff.size() );
+
+      text.Serialize( DelphiX::textAPI::dump_as::Tags(
+        DelphiX::textAPI::dump_as::MakeOutput( stdout ) ) );
+    }
+  }
+} );
---
+++ tests/readers/test-zipfile.cpp
@@ -0,0 +1,34 @@
+# include "../../readers/read-zip.hpp"
+# include <mtc/test-it-easy.hpp>
+
+auto  LoadFile( const char* path ) -> std::vector<char>
+{
+  std::vector<char> load;
+  char              buff[1024];
+  auto              file = fopen( path, "rb" );
+
+  for ( auto read = fread( buff, 1, sizeof(buff), file ); read > 0; read = fread( buff, 1, sizeof(buff), file ) )
+    load.insert( load.end(), buff, buff + read );
+
+  fclose( file );
+
+  return load;
+}
+
+TestItEasy::RegisterFunc  test_zipfile( []()
+{
+  TEST_CASE( "zipfile/reader" )
+  {
+    SECTION( "zip may be opened, listed and read" )
+    {
+      auto  buf = LoadFile( "/home/keva/Downloads/Libruks/Архивы Либрусек/fb2-793780-793916.zip" );
+
+      palmira::minizip::Read( []( const DelphiX::Slice<const char>&, const std::vector<std::string>& names )
+        {
+          for ( size_t i = 0; i != names.size(); ++i )
+            fprintf( stdout, i != 0 ? "->%s" : "%s", names[i].c_str() );
+          fputc( '\n', stdout );
+        }, buf );
+    }
+  }
+} );
