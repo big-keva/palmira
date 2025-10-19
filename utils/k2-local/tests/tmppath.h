b6f97716fcc2c25a6401f1184550e3e29c838062
Keva <andrey.kovalenko@myoffice.team>
2025-10-19 13:36:34 +0300

    newly added

---
+++ utils/k2-local/tests/tmppath.h
@@ -0,0 +1,7 @@
+# if !defined( __DelphiX_tests_tmppath_h__ )
+# define __DelphiX_tests_tmppath_h__
+# include <string>
+
+auto  GetTmpPath() -> std::string;
+
+# endif   // !__DelphiX_tests_tmppath_h__
