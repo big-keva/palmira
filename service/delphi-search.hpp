1c34a5f5e812525d29e82470a1ebee557420f076
Keva <keva@rambler.ru>
2025-11-29 16:08:23 +0300

    cosmetic changes

--- service/delphi-search.hpp
+++ service/delphi-search.hpp
@@ -1,25 +1,25 @@
 # if !defined( __palmira_delphi_search_hpp__ )
 # define __palmira_delphi_search_hpp__
-# include "DelphiX/context/processor.hpp"
+# include "structo/context/processor.hpp"
 # include "service.hpp"
 
 namespace palmira {
 
-  using namespace DelphiX;
+  using namespace structo;
 
   using FnContents = std::function<mtc::api<IContents>(
     const mtc::span<const mtc::span<const context::Lexeme>>&,
     const mtc::span<const DeliriX::MarkupTag>&, FieldHandler& )>;
 
-  class DelphiXService
+  class StructoService
   {
     class data;
 
   public:
-    auto  Set( FnContents ) -> DelphiXService&;
-    auto  Set( mtc::api<IContentsIndex> ) -> DelphiXService&;
-    auto  Set( context::Processor&& ) -> DelphiXService&;
-    auto  Set( const context::Processor& ) -> DelphiXService&;
+    auto  Set( FnContents )               -> StructoService&;
+    auto  Set( mtc::api<IContentsIndex> ) -> StructoService&;
+    auto  Set( context::Processor&& )     -> StructoService&;
+    auto  Set( const context::Processor& ) -> StructoService&;
 
   public:
     auto  Create() -> mtc::api<IService>;
