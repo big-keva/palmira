f6a0be6b8f75479a5058222d9d95b9ca503e0f0f
Keva <keva@rambler.ru>
2025-12-17 13:50:00 +0300

    newly added

---
+++ grpcapi/server.hpp
@@ -0,0 +1,11 @@
+# if !defined( __palmira_grpcttp_server_hpp__ )
+# define __palmira_grpcttp_server_hpp__
+# include "../server.hpp"
+# include "../service.hpp"
+
+namespace grpcapi
+{
+  auto  CreateServer( mtc::api<palmira::IService>, uint16_t port ) -> mtc::api<palmira::IServer>;
+}
+
+# endif // !__palmira_grpcttp_server_hpp__
