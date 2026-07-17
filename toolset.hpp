db7250c5746a282eb7060925cb7e364237b75c15
Keva <keva@rambler.ru>
2026-07-17 13:50:28 +0300

    builtin: supported for servers

--- toolset.hpp
+++ toolset.hpp
@@ -5,6 +5,26 @@
 namespace palmira
 {
   auto  Immediate( const mtc::zmap&, IService::NotifyFn = {} ) -> mtc::api<IService::IPending>;
+
+ /*
+  * void  AddModule( ... )
+  *
+  * Регистрирует функцию по имени и имени модуля
+  */
+  void  AddModule(
+    const char* moduleName,
+    const char* methodName,
+    void*       methodAddr );
+
+ /*
+  * void  GetModule( ... )
+  *
+  * Ищет функцию по имени и имени модуля
+  */
+  void* GetModule(
+    const char* moduleName,
+    const char* methodName );
+
 }
 
 # endif // !__palmira_toolset_hpp__
