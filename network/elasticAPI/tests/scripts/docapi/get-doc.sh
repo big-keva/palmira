244f013ed07bf32a2c5d74688c56e2d99bc27d42
Vitaly Buganin <vitaly.bulganin@gmail.com>
2026-07-20 11:55:13 +0300

    * Commented uncheck requests

--- network/elasticAPI/tests/scripts/docapi/get-doc.sh
+++ network/elasticAPI/tests/scripts/docapi/get-doc.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+DOCAPI_DOC_ID=1
+
 # Adding a document
-curl -X POST "127.0.0.1:9300/tests/_doc/1" -H 'Content-Type: application/json' -d '
+curl -X POST "127.0.0.1:9300/tests/_doc/${DOCAPI_DOC_ID}" -H 'Content-Type: application/json' -d '
 {
   "title": "Первая запись",
   "author": "Иван Иванович",
@@ -9,18 +11,17 @@ curl -X POST "127.0.0.1:9300/tests/_doc/1" -H 'Content-Type: application/json' -
   "annotation": {
     "name": "Vitaly",
     "identity": {
-      "type": "pasport"
+      "type": "passport"
     }
   }
 }
 '
-DOCAPI_DOC_ID=1
 
 echo "Getting a document by id: ${DOCAPI_DOC_ID}"
-curl -X POST "127.0.0.1:9300/tests/_doc/${DOCAPI_DOC_ID}"
+curl -X GET "127.0.0.1:9300/tests/_doc/${DOCAPI_DOC_ID}"
 
-echo "Getting a document by id and title: ${DOCAPI_DOC_ID}"
-curl -X POST "127.0.0.1:9300/tests/_doc/${DOCAPI_DOC_ID}?title=Первая запись"
+# echo "Getting a document by id and title: ${DOCAPI_DOC_ID}"
+# curl -X GET "127.0.0.1:9300/tests/_doc/${DOCAPI_DOC_ID}?title=Первая запись"
 
-echo "Getting a document by id and _source: ${DOCAPI_DOC_ID}"
-curl -X POST "127.0.0.1:9300/tests/_doc/${DOCAPI_DOC_ID}?_source=Первая запись"
+# echo "Getting a document by id and _source: ${DOCAPI_DOC_ID}"
+# curl -X GET "127.0.0.1:9300/tests/_doc/${DOCAPI_DOC_ID}?_source=Первая запись"
